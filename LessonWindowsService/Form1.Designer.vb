3fe6efd5ac4f3233cf9b1069403304ab0b676073
Mikatsuki Sakura <sakuramikatsuki@gmail.com>
2015-09-26 13:08:44 +0900

    first commit.

---
+++ LessonWindowsService/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.cboSelSvc = New System.Windows.Forms.ComboBox()
+        Me.lblStats = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cboSelSvc
+        '
+        Me.cboSelSvc.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cboSelSvc.FormattingEnabled = True
+        Me.cboSelSvc.Location = New System.Drawing.Point(12, 24)
+        Me.cboSelSvc.Name = "cboSelSvc"
+        Me.cboSelSvc.Size = New System.Drawing.Size(570, 20)
+        Me.cboSelSvc.TabIndex = 0
+        '
+        'lblStats
+        '
+        Me.lblStats.AutoSize = True
+        Me.lblStats.Location = New System.Drawing.Point(12, 9)
+        Me.lblStats.Name = "lblStats"
+        Me.lblStats.Size = New System.Drawing.Size(23, 12)
+        Me.lblStats.TabIndex = 1
+        Me.lblStats.Text = "***"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 3000
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 79)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(569, 20)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(124, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(23, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "***"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 131)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.lblStats)
+        Me.Controls.Add(Me.cboSelSvc)
+        Me.Name = "Form1"
+        Me.Text = "Check to Windows Service"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cboSelSvc As System.Windows.Forms.ComboBox
+    Friend WithEvents lblStats As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ LessonWindowsService/Form1.vb
@@ -0,0 +1,123 @@
+﻿''' <summary>
+''' サービスの状態を確認
+''' http://codezine.jp/article/detail/157
+''' 
+''' </summary>
+''' <remarks></remarks>
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim Clist As New ArrayList() 'コレクションArrayListを宣言 '配列でもOKかな
+
+        Dim Svcs() As ServiceProcess.ServiceController 'WindowsService取得用
+        Svcs = ServiceProcess.ServiceController.GetServices '返値はServiceController型の配列です。
+
+        'ServiceController→String
+        For Each Svc As ServiceProcess.ServiceController In Svcs '配列を1つずつ処理します。1つずつやるしかないようです。
+            cboSelSvc.Items.Add(Svc.DisplayName) 'DisplayNameはString
+            Clist.Add(Svc.DisplayName)
+        Next
+
+        'cboSelSvc.Items.AddRange(Svcs.ToArray) '←このやり方はNGでした。
+        'cboSelSvc.Text = cboSelSvc.Items.Item(0)←これもNGになります。結果StringでないよとNGになります。
+
+        Clist.Sort() '整列
+
+        ComboBox1.Items.AddRange(Clist.ToArray) 'コンボボックスに追加
+
+        '基本の機能で目的は達成するがこちらも頑張ってみよう →OK
+        'For Each cs As String In Clist
+        '    ComboBox1.Items.Add(cs)
+        'Next
+
+        cboSelSvc.Text = cboSelSvc.Items.Item(0) '最初の行を表示させておく
+        'cboSelSvc.Text = "avast! Antivirus" '指名して表示させておくのもOK
+
+        ComboBox1.Text = ComboBox1.Items.Item(0)
+
+        Timer1.Start()
+        GetStatus() '追加
+
+    End Sub
+
+    Private Sub GetStatus()
+        Dim ServiceStat As New ServiceProcess.ServiceController
+        ServiceStat.DisplayName = cboSelSvc.Text
+
+        Try
+            'Label1.Text = ServiceStat.Status 'これだと数値が表示されますが
+            Label1.Text = ServiceStat.Status.ToString 'ToStringで意味のある文字列が表示されます。便利だね。
+
+        Catch ex As Exception
+            Label1.Text = "***"
+
+        End Try
+
+        Try
+            Select Case ServiceStat.Status
+                Case ServiceProcess.ServiceControllerStatus.ContinuePending
+                    lblStats.Text = "再開中"
+                Case ServiceProcess.ServiceControllerStatus.Paused
+                    lblStats.Text = "一時停止中"
+                Case ServiceProcess.ServiceControllerStatus.PausePending
+                    lblStats.Text = "一時停止状態に移行中"
+                Case ServiceProcess.ServiceControllerStatus.Running
+                    lblStats.Text = "動作中"
+                Case ServiceProcess.ServiceControllerStatus.StartPending
+                    lblStats.Text = "開始中"
+                Case ServiceProcess.ServiceControllerStatus.Stopped
+                    lblStats.Text = "停止中"
+                Case ServiceProcess.ServiceControllerStatus.StopPending
+                    lblStats.Text = "停止状態に移行中"
+            End Select
+        Catch ex As Exception
+            lblStats.Text = "サービス名エラー"
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Serviceの状態を返す
+    ''' </summary>
+    ''' <param name="Stext"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function GetStatus2(Stext As String)
+
+        Dim ServiceStat As New ServiceProcess.ServiceController
+        ServiceStat.DisplayName = Stext
+
+        Try
+            Select Case ServiceStat.Status
+                Case ServiceProcess.ServiceControllerStatus.ContinuePending
+                    lblStats.Text = "再開中"
+                Case ServiceProcess.ServiceControllerStatus.Paused
+                    lblStats.Text = "一時停止中"
+                Case ServiceProcess.ServiceControllerStatus.PausePending
+                    lblStats.Text = "一時停止状態に移行中"
+                Case ServiceProcess.ServiceControllerStatus.Running
+                    lblStats.Text = "動作中"
+                Case ServiceProcess.ServiceControllerStatus.StartPending
+                    lblStats.Text = "開始中"
+                Case ServiceProcess.ServiceControllerStatus.Stopped
+                    lblStats.Text = "停止中"
+                Case ServiceProcess.ServiceControllerStatus.StopPending
+                    lblStats.Text = "停止状態に移行中"
+            End Select
+        Catch ex As Exception
+            lblStats.Text = "サービス名エラー"
+        End Try
+
+
+    End Function
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        GetStatus()
+    End Sub
+
+    Private Sub cboSelSvc_SelectedValueChanged(sender As Object, e As EventArgs) Handles cboSelSvc.SelectedValueChanged
+        GetStatus()
+    End Sub
+End Class
